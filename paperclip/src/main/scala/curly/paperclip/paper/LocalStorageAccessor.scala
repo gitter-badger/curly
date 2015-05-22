becdd3376466271981e6ba04d196ba37f15fe8f5
Joao Evangelista <evangelistajoaop@gmail.com>
2015-05-22 00:14:32 -0300

    fixed headers

--- paperclip/src/main/scala/curly/paperclip/paper/LocalStorageAccessor.scala
+++ paperclip/src/main/scala/curly/paperclip/paper/LocalStorageAccessor.scala
@@ -1,3 +1,18 @@
+/*
+ *        Copyright 2015 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package curly.paperclip.paper
 
 import java.nio.file.{Files, Paths}
@@ -7,7 +22,7 @@ import java.nio.file.{Files, Paths}
  */
 class LocalStorageAccessor extends StorageAccessor {
 
-  val systemHome = System.getenv("HOME")
+  val systemHome = System.getenv("HOMEPATH")
 
   override def rawContent(path: String): String = {
     val path = java.nio.file.Paths.get(systemHome, path)
